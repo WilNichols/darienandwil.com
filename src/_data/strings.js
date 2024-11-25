bb16dd6f410f5b30f8fe163367c8684d7dec8fc1
Wil Nichols <hi@wilnichols.design>
2024-11-25 15:28:46 -0600

    RSVP-no page and form test

--- src/_data/strings.js
+++ src/_data/strings.js
@@ -2,5 +2,7 @@ export default function () {
   return {
     pipe: ' | ',
     routeVia: 'Route via ',
+    noRSVPTitle: 'You’ll be missed!',
+    noRSVPBody: 'But seriously, thanks for letting us know.'
   }
 }
