8a951e75cb948ca1ac8db0a1ae43f857799aa4b2
Wil Nichols <hi@wilnichols.design>
2024-11-30 15:58:40 -0600

    debug

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,6 +2,7 @@ export default function () {
   return {
     lang: 'en',
     url: 'https://darienandwil.com',
-    title: 'Darien & Wil'
+    title: 'Darien & Wil',
+    rsvpForm1: 'rsvp-party-2,'
   }
 }
