bbad05b4f39f0d89b7461d2579c60cd41312abeb
Wil Nichols <hi@wilnichols.design>
2024-11-30 19:21:44 -0600

    testing

--- src/_data/rsvpForms.js
+++ src/_data/rsvpForms.js
@@ -13,5 +13,27 @@ export default async function () {
     },
   });
 
+ //  let url2 = 'https://api.netlify.com/api/v1/sites/' + process.env.NETLIFY_SITE_KEY + '/forms';
+ //  
+ //  let json2 = await Fetch(url2, {
+ //    duration: '1m',
+ //    type: 'json',
+ //    fetchOptions: {
+ //      headers: {
+ //        'Authorization': 'Bearer ' + process.env.NETLIFY_KEY
+ //      },
+ //    },
+ //  });
+ //  
+ //  // console.log(json2);
+ //  let arr = [];
+ // for (let obj in json2) {
+ //   arr.push(json2[obj].name);
+ //   fetch('https://api.netlify.com/api/v1/sites/' + process.env.NETLIFY_SITE_KEY + '/forms/' + json2[obj].name, {
+ //     method: 'DELETE',
+ //     headers: {Authorization: 'Bearer ' + process.env.NETLIFY_KEY}
+ //   })
+ // }
+ // console.log(arr);
   return json;
 };
