cc8f8879cb054a63550103a1aef46580687bc671
Wil Nichols <hi@wilnichols.design>
2024-11-30 23:58:40 -0600

    debug

--- src/_data/rsvpForms.js
+++ src/_data/rsvpForms.js
@@ -1,17 +1,17 @@
 import Fetch from '@11ty/eleventy-fetch';
 
 export default async function () {
-  let url = 'https://api.netlify.com/api/v1/sites/' + process.env.NETLIFY_SITE_KEY + '/submissions';
+//   let url = 'https://api.netlify.com/api/v1/sites/' + process.env.NETLIFY_SITE_KEY + '/submissions';
+// 
+//   let json = await Fetch(url, {
+//     duration: '1m',
+//     type: 'json',
+//     fetchOptions: {
+//       headers: {
+//         'Authorization': 'Bearer ' + process.env.NETLIFY_KEY
+//       },
+//     },
+//   });
 
-  let json = await Fetch(url, {
-    duration: '1m',
-    type: 'json',
-    fetchOptions: {
-      headers: {
-        'Authorization': 'Bearer ' + process.env.NETLIFY_KEY
-      },
-    },
-  });
-
-  return json;
+  return {};
 };
