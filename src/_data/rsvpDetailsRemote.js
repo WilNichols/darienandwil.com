bf899359ab8754634e97e2a9ac38d36785ffc012
Wil Nichols <hi@wilnichols.design>
2024-12-01 02:38:24 -0600

    more

---
+++ src/_data/rsvpDetailsRemote.js
@@ -0,0 +1,15 @@
+import Fetch from '@11ty/eleventy-fetch';
+
+export default async function () {
+  let url = 'https://api.netlify.com/api/v1/forms/' + process.env.DETAILS_FORM + '/submissions';
+
+  return await Fetch(url, {
+    duration: '1m',
+    type: 'json',
+    fetchOptions: {
+      headers: {
+        'Authorization': 'Bearer ' + process.env.NETLIFY_KEY
+      },
+    },
+  });
+};
